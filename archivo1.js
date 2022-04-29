f036923c7092c1d6335356eed2a7d35451c634b4
nataliaArakaki <natalia.arakaki.prof@gmail.com>
2022-04-29 09:36:41 -0300

    Prueba de bajar y modificar archivos Natu

--- archivo1.js
+++ archivo1.js
@@ -1,2 +1,3 @@
 console.log ("Hola Fer");
-console.log ("hola Nati, estoy probando cambiar tu archivo tuyo")
+console.log ("hola Nati, estoy probando cambiar tu archivo tuyo");
+console.log ("Buen día Fer, confirmo que funciono");
--- archivo4.js
+++ archivo4.js
@@ -1,1 +1,2 @@
 console.log("hola Nati");
+console.log ("ya estamos para empezar a buscar clientes o es mucho? Jaja");
